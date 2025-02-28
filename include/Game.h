27cb9c6862ff52c7f9ec311419dae773bb087b53
Aditya Thakur <thakuradityadev9336@gmail.com>
2025-02-28 19:38:46 +0530

    Add header file for abstract class

---
+++ include/Game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+
+//Adding a Abstract Base class to act as an interface for all the games
+class Base{
+public:
+   virtual void play() = 0;
+   virtual ~Game() {} 
+};
+
+#endif GAME_H
+
+
+// Game is an abstract base class that other games will inherit from.
+// play() is a pure virtual function, meaning every game must implement it.
+// ~Game() is a virtual destructor to ensure proper cleanup when deleting derived objects.
