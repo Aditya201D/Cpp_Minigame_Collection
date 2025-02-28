75c0763a71623c0538da5fcb5a3d9af543002529
Aditya Thakur <thakuradityadev9336@gmail.com>
2025-02-28 22:37:06 +0530

    Add access to Num Guessing game

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <Game.h>
-
+#include "NumberGuessingGame.h"
 
 void ShowMenu(){
     std::cout<<"Welcome to the mini-game collection, user!"<<std::endl;
@@ -16,10 +16,20 @@ int main(){
         std::cin>>choice_of_game;
 
         if (choice_of_game = 2){
-            std::cout<<"Sad to see you go! Exiting the game console..."<<std::endl;
+            std::cout<<"Sad to see you go! Exiting the game console...";
         }
         
-        std::cout<<"Your game will now start!"<<std::endl;      //This message prints after the user has entered their choice other than the exit option
+        if (choice_of_game = 1){
+            std::cout<<"You chose Number Guessing Game? Alright, let's dive into a game of pure luck!";
+            NumberGuessingGame game;
+            game.play();
+        }
+        else{
+            std::cout<<"Invalid output";
+        }
+
+        std::cout<<std::endl;
+        std::cout<<"Your game will now start!";     //This message prints after the user has entered their choice other than the exit option
     }
     return 0;
 }
