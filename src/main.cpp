7c6c5f086cda9bca1d868ec6e521bd2e88e48a97
Aditya Thakur <thakuradityadev9336@gmail.com>
2025-02-28 19:56:00 +0530

    Add starting interface for main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <Game.h>
+
+
+void ShowMenu(){
+    std::cout<<"Welcome to the mini-game collection, user!"<<std::endl;
+    std::cout<<"***********LIST OF AVAILABLE GAMES*************"<<std::endl;
+    std::cout<<"1. Number Guessing Game\n2. Exit\n";
+    std::cout<<"Choose your game!: "<<std::endl;
+}
 
 int main(){
-    std::cout<<"Welcome to the mini-game collection, user!"<<std::endl;
+    int choice_of_game;
+    while (true){
+        ShowMenu();
+        std::cin>>choice_of_game;
+
+        if (choice_of_game = 2){
+            std::cout<<"Sad to see you go! Exiting the game console..."<<std::endl;
+        }
+        
+        std::cout<<"Your game will now start!"<<std::endl;      //This message prints after the user has entered their choice other than the exit option
+    }
     return 0;
 }
